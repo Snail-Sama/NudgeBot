3d599e3e955dae1023c77f631655793fe3721a41
Alexander Smeulders <alexs23@bu.edu>
2025-01-07 13:46:47 -0500

    initial commit with basic bot

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+
+client = discord.Client()
+
+with open('token.txt') as file:
+    token = file.read()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}.format(client)')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('.hello'):
+        await message.channel.send('shut up :Love:')
+
+client.run(token)
