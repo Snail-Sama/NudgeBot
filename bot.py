59bbcc5fcc2df92d2a6212f84cd34ae00a038a70
cfine <cjf5748@g.rit.edu>
2025-01-09 15:09:23 -0500

    reformating and setup before creating feature branch

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import settings
+
+logger = settings.logging.getLogger("bot") #
+
+def run():
+    intents = discord.Intents.all()
+    bot = commands.Bot(command_prefix=".", intents = intents)
+
+    @bot.event
+    async def on_ready():
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+
+        for cogs_file in settings.COGS_DIR.glob("*.py"):
+            if cogs_file != "__init__.py":
+                await bot.load_extension(f"cogs.{cogs_file.name[:-3]}")
+    
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+    
+
+if __name__ == "__main__":
+    run()
