a6707d3e6a5ba72b0f36a36e8b2e429bc937a5cb
asmeulders <alexs23@bu.edu>
2025-06-13 14:28:35 -0400

    installing dependencies

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /root/NudgeBot/venv
+set -gx VIRTUAL_ENV /home/asmeu/NudgeBot/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
